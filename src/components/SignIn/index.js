9db982abb35ebf94f8721a2dff7cc38b89d23b64
OmarSouza <omarsouza565@gmail.com>
2022-05-12 23:07:41 -0300

    TesteStyle

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,48 +2,47 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
 
-function SignIn({ navigation }) {
-    return (
-        <View style={styles.componentSignIn}>
-            <View style={styles.containerSignIn}>
-                <Image source={require('../../assets/logo.png')} style={styles.logoSignIn} />
+class SignIn extends React.Component {
+    render() {
+        return (
+            <View style={styles.componentSignIn}>
+                <View style={styles.containerSignIn}>
+                    <Image source={require('../../assets/logo.png')} style={styles.logoSignIn} />
 
-                <View style={styles.containerInputSignIn}>
-                    <Text>Usuario: </Text>
-                    <TextInput style={styles.textInputSignIn}/>
+                    <View style={styles.formSignIn}>
+                        <Text style={styles.textSignIn}>Usuario: </Text>
+                        <TextInput />
 
-                    <Text>Senha: </Text>
-                    <TextInput/>
-                </View>
+                        <Text style={styles.textSignIn}>Senha: </Text>
+                        <TextInput />
 
-                <View style={styles.containerBotaoSignIn}>
-                    <TouchableOpacity>
-                        <Text>Acessar</Text>
-                    </TouchableOpacity>
+                        <View >
+                            <TouchableOpacity>
+                                <Text>Acessar</Text>
+                            </TouchableOpacity>
 
-                    <TouchableOpacity>
-                        <Text>Cadastrar</Text>
-                    </TouchableOpacity>
+                            <TouchableOpacity>
+                                <Text>Cadastrar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
             </View>
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     componentSignIn: {
-        flex: 1
+        padding: 50
     },
 
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    containerSignIn: {
+
     },
 
     logoSignIn: {
-        marginBottom: 50
+
     }
 });
-
 export default SignIn;
